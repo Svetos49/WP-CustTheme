e4ab4770e6db46c705a9e4fc38a16acfbb626973
Svetoslav Slavov <75615836+Svetos49@users.noreply.github.com>
2023-10-26 17:17:42 +0300

    Update search.php

--- wp-content/themes/creativeweb/search.php
+++ wp-content/themes/creativeweb/search.php
@@ -33,6 +33,7 @@ get_header();
 				 * If you want to overload this in a child theme then include a file
 				 * called content-search.php and that will be used instead.
 				 */
+				the_title();
 				the_content();
 
 			endwhile;

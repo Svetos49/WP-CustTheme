2117d684a0f247074e50c657663f3234b2f479e5
Svetoslav Slavov <75615836+Svetos49@users.noreply.github.com>
2023-11-10 21:52:26 +0200

    Update functions.php

--- wp-content/themes/creativeweb/functions.php
+++ wp-content/themes/creativeweb/functions.php
@@ -51,6 +51,56 @@ function creativeweb_custom_search($form) {
 }
 add_filter('get_search_form', 'creativeweb_custom_search');
 
+function creativeweb_register_post_type() {
+  $args = array(
+	'label' => esc_html__('Cars', 'creativeweb'),
+	 'labels' => array(
+		'name'                  => esc_html_x( 'Cars', 'Post type general name', 'creativeweb' ),
+		'singular_name'         => esc_html_x( 'Car', 'Post type singular name', 'creativeweb' ),
+		'menu_name'             => esc_html_x( 'Cars', 'Admin Menu text', 'creativeweb' ),
+		'name_admin_bar'        => esc_html_x( 'Car', 'Add New on Toolbar', 'creativeweb' ),
+		'add_new'               => esc_html__( 'Add New', 'creativeweb' ),
+		'add_new_item'          => esc_html__( 'Add New Car', 'creativeweb' ),
+		'new_item'              => esc_html__( 'New Car', 'creativeweb' ),
+		'edit_item'             => esc_html__( 'Edit Car', 'creativeweb' ),
+		'view_item'             => esc_html__( 'View Car', 'creativeweb' ),
+		'all_items'             => esc_html__( 'All Cars', 'creativeweb' ),
+		'search_items'          => esc_html__( 'Search Cars', 'creativeweb' ),
+		'parent_item_colon'     => esc_html__( 'Parent Cars:', 'creativeweb' ),
+		'not_found'             => esc_html__( 'No Cars found.', 'creativeweb' ),
+		'not_found_in_trash'    => esc_html__( 'No Cars found in Trash.', 'creativeweb' ),
+		'featured_image'        => esc_html_x( 'Car Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'creativeweb' ),
+		'set_featured_image'    => esc_html_x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'creativeweb' ),
+		'remove_featured_image' => esc_html_x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'creativeweb' ),
+		'use_featured_image'    => esc_html_x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'creativeweb' ),
+		'archives'              => esc_html_x( 'Car archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'creativeweb' ),
+		'insert_into_item'      => esc_html_x( 'Insert into Car', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'creativeweb' ),
+		'uploaded_to_this_item' => esc_html_x( 'Uploaded to this Car', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'creativeweb' ),
+		'filter_items_list'     => esc_html_x( 'Filter Cars list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'creativeweb' ),
+		'items_list_navigation' => esc_html_x( 'Cars list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'creativeweb' ),
+		'items_list'            => esc_html_x( 'Cars list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'creativeweb' ),
+	 ), 
+	 'supports' => array('title', 'editor', 'author', 'thumbnail'),
+	 'public' => true,
+	 'publicly_queryable' => true,
+	 'show_ui' => true,
+	 'show_in_menu' => true,
+	 'has_archive' => true,
+	);	
+  register_post_type('car', $args);
+}
+add_action('init', 'creativeweb_register_post_type');
+
+
+
+
+
+
+
+
+
+
+
 
 
 if ( ! defined( '_S_VERSION' ) ) {

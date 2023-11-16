c02265b4d1379f43ff235a0f522d13049c7bc31b
Svetoslav Slavov <75615836+Svetos49@users.noreply.github.com>
2023-11-16 16:01:56 +0200

    Update archive.php

--- wp-content/themes/creativeweb/archive.php
+++ wp-content/themes/creativeweb/archive.php
@@ -10,41 +10,24 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
+	div>
+	<header class="page-header">
+		<?php
+		the_archive_title('<h1 class="page-title">', '</h1>');
+		the_archive_description('<div class="archive-description">', '</div>');
 		?>
+	</header>
 
-	</main><!-- #main -->
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+			<?php get_template_part('partials/content'); ?>
+
+		<?php endwhile;
+	else : ?>
+		<?php get_template_part('partials/content', 'none'); ?>
+
+	<?php endif; ?>
+</div>
 
 <?php
 get_sidebar();

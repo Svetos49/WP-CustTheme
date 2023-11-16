973a1850c21a108eef0f210ea3e5f7d1d96153c6
Svetoslav Slavov <75615836+Svetos49@users.noreply.github.com>
2023-11-16 15:54:14 +0200

    Update index.php

--- wp-content/themes/creativeweb/index.php
+++ wp-content/themes/creativeweb/index.php
@@ -15,42 +15,16 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<div>
+		<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 
-		<?php
-		if ( have_posts() ) :
+		<?php get_template_part('partials/content'); ?>
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
+		<?php endwhile; else : ?>
+			<?php get_template_part('partials/content', 'none'); ?>
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
+		 <?php endif; ?>	
+	</div>
 
 <?php
 get_sidebar();
